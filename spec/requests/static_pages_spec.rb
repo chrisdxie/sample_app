c62c47cc94ef4612593b2b8922ba817b1fbb8ef4
chrisdxie <chrisdxie@gmail.com>
2012-12-26 13:44:42 -0800

    Adds a a full_title helper (application)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,9 +9,14 @@ describe "StaticPages" do
 	  page.should have_selector('h1', :text => 'Sample App')
     end
 
-	it "should have as title 'Home'" do
+	it "should have as title 'Ruby on Rails Tutorial Sample App'" do
 	  visit '/static_pages/home'
-	  page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | Home')
+	  page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App')
+	end
+
+	it "should not have 'Home' as part of title" do
+	  visit '/static_pages/home'
+	  page.should_not have_selector('title', :text => '| Home')
 	end
 
   end
